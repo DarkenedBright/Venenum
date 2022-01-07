f2f47f93336bb1b09cb1acb4b48f07cf5b8286a1
Erik Ostien <ostienerik@gmail.com>
2022-01-07 10:19:07 -0700

    Untrack all VSCode files and create input parsing mechanism

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Venenum - A UCI Chess Engine\n";
+
+    //Read UCI Protocol from standard input
+    std::string inputLine{};
+    while(true)
+    {
+        std::string inputLine;
+        std::getline(std::cin, inputLine, '\n');
+        std::stringstream inputLineStream{inputLine};
+
+        std::vector<std::string> inputArray{};
+
+        std::string inputSegment{};
+
+        while(std::getline(inputLineStream, inputSegment, ' '))
+        {
+            inputArray.push_back(inputSegment);
+        }
+    }
 
     return 0;
 }

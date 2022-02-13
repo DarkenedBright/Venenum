5cfa6c2a91f05ebc0396f5b9d4775d6dce70d789
Erik-Ostien <ostienerik@gmail.com>
2022-02-13 14:41:51 -0700

    Switch to using Bitboards for Board Representation

--- src/venenum.cpp
+++ src/venenum.cpp
@@ -15,6 +15,9 @@ int main()
     Position position { STANDARD_START_FEN };
     position.print();
 
+    Position position2 { "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2" };
+    position2.print();
+
     readConsole();
 
     return 0;
